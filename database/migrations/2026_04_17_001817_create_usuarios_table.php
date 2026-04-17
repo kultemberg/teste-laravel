cd1db0d1fb878468450653b05f4082d19f564c94
Rafael Kultemberg <kultemberg@gmail.com>
2026-04-16 22:23:29 -0300

    Configuração de autenticação com Usuario e ajuste migration de usuarios

--- database/migrations/2026_04_17_001817_create_usuarios_table.php
+++ database/migrations/2026_04_17_001817_create_usuarios_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,10 @@ return new class extends Migration
     {
         Schema::create('usuarios', function (Blueprint $table) {
             $table->id();
+            $table->string('nome');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->string('role');
             $table->timestamps();
         });
     }
